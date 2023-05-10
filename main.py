c59b1fc43526d739d9a289b21191c1957ee74224
Sta4 <youzzerr@gmail.com>
2023-05-10 20:33:45 +0500

    add new methods /start and /help

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
-
 bot.set_my_commands([
     telebot.types.BotCommand("/start", "Для начала работы"),
     telebot.types.BotCommand("/help", "Инструкция использования"),
 ])
 
-start_message = "Для использования \n\n <b>команды</b>\n\n" \
-                "/start - Для начала работы бота\n"
+start_message = "<b>команды</b> Для использования\n\n" \
+                "/start - Для начала работы бота\n" \
+                "/help - Помощь в использовании"
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    chat_id = int(message.chat.id)
+    bot.send_message(chat_id, f"Привет")
+
+
+@bot.message_handler(commands=['help'])
+def help_method(message):
+    chat_id = int(message.chat.id)
+    bot.send_message(chat_id, f"{start_message}", parse_mode='html')
+
 
 if __name__ == "__main__":
-    pass
+    bot.polling(none_stop=True)
